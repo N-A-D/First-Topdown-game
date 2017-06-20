a2a912b560c2e488bd5e13b4d432a996c10bd532
Ned Austin Datiles <ned.austin.datiles@ryerson.ca>
2017-06-19 21:06:36 -0400

    Update to pathfinding. Utilizing my very own heap!

--- heap.py
+++ heap.py
@@ -1,8 +1,23 @@
 def min_heap_push(heap, item, cost):
+    """
+    Inserts an item into the heap
+    and reheapifies according to min cost
+    :param heap: The list of items
+    :param item: The item to insert
+    :param cost: The item priority
+    :return: None
+    """
     heap.append((cost, item))
     min_heap(heap)
 
 def min_heap_pop(heap):
+    """
+    Pops an item from the heap
+    and reheapifies according to
+    min cost
+    :param heap: The list to pop an item from
+    :return: None
+    """
     last_item = heap.pop()
     if heap:
         returnitem = heap[0]
@@ -12,6 +27,11 @@ def min_heap_pop(heap):
     return last_item
 
 def min_heap(heap):
+    """
+    Creates a min heap given a list of items with their priorities.
+    :param heap: The list to "heapify"
+    :return: None
+    """
     n = len(heap)
     for idx in range(n // 2, 0, -1):
         key = idx
@@ -29,10 +49,22 @@ def min_heap(heap):
         heap[key - 1] = current
 
 def max_heap_push(heap, item, cost):
+    """
+    Pushes an item into the list and reheapifies that list
+    :param heap: The list of items
+    :param item: The item to insert
+    :param cost: The item priority
+    :return: None
+    """
     heap.append((cost, item))
     max_heap(heap)
 
 def max_heap_pop(heap):
+    """
+    Pops an item from the list and reheapifies that list
+    :param heap: The list of items
+    :return: None
+    """
     last_item = heap.pop()
     if heap:
         returnitem = heap[0]
@@ -43,6 +75,11 @@ def max_heap_pop(heap):
 
 
 def max_heap(heap):
+    """
+    Creates a max heap given a list of items with their priorities.
+    :param heap: The list of items
+    :return: None
+    """
     n = len(heap)
     for idx in range(n // 2, 0, -1):
         key = idx
